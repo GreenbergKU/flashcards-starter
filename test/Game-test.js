be7bb8dec64ec91917978e392aceba04cb234b35
Suzie Q <suzieqwolf@gmail.com>
2020-12-07 14:10:50 -0700

    Sets up Game-test.js for chai & adds file paths.

---
+++ test/Game-test.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const data = require('../src/data');
+const prototypeQuestions = ('../src/data.prototypeData');
+const util = require('../src/util');
+const Card = require('../src/Card');
+
+
+
+
+
