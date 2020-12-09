6b2e148d6446ffa398de435df964c9eda8300548
Suzie Q <suzieqwolf@gmail.com>
2020-12-09 01:27:43 -0700

    Adds code for Game class with Game-tests (pass).

--- src/Game.js
+++ src/Game.js
@@ -1,19 +1,24 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
-const Card = require('./Card');
+
+//const Card = require('./Card');
+//const Deck = require('./Deck');
+//const Round = require('./Round'); 
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.welcomeMessage;
+  };
 
   printMessage(deck, round) {
-      console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
------------------------------------------------------------------------`)
-  }
+      
+    console.log(this.welcomeMessage = `This is round #${round.turnsCount} of FlashCards! You are playing with ${deck.countCards()} cards.-----------------------------------------------------------------------`);
+  };
 
   printQuestion(round) {
       util.main(round);
-  }
+  };
 }
 
 module.exports = Game;
--- test/Game-test.js
+++ test/Game-test.js
@@ -2,11 +2,29 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const data = require('../src/data');
-const prototypeQuestions = ('../src/data.prototypeData');
-const util = require('../src/util');
-const Card = require('../src/Card');
+const prototypeQuestions = data.prototypeData;
+//const util = require('../src/util');
+//const Card = require('../src/Card');
+const Game = require('../src/Game');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 
+describe('Game', function() {
+  //const deck = new Deck(prototypeQuestions);
+  describe('printMessage', function() {
+    it('should be a function', function() {
+      let game = new Game;
+      let deck = new Deck(prototypeQuestions);
+      let round = new Round(deck);
+
+      expect(game.printMessage).to.be.a('function');
+      expect(game.printMessage(deck, round)).to.equal(undefined);
+      expect(game.welcomeMessage).to.equal(`This is round #${round.turnsCount} of FlashCards! You are playing with ${deck.countCards()} cards.-----------------------------------------------------------------------`);    
+    });
+  });
+
+});
 
 
 
