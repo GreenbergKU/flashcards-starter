8642b44f2210bc84259044af7957c26c3d506e6d
SuzieQ <suzieqwolf@gmail.com>
2020-12-10 21:34:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
 const http = require('http');
+const Game = require('./src/Game');
+const data = require('./src/data');
+const prototypeQuestions = data.prototypeData;
+const game = Game;
+let app = http.createServer();
+
+// Start the server on port 3000
+app.listen(3000, '127.0.0.1');
+console.log('Node server running on port 3000');   
+
+// Setup game
+setupGame();
+
+function setupGame() {
+  game = new Game(prototypeQuestions);
+ 
+  // Start game
+  game.start();
+};
+
+function checkRedo {
+  if(game.currentRound.redo) {
+    // if(game.currentRound.findPercentCorrect() < 90) {
+      // round.redo = true;
+    // };
+
+    setupGame();
+  };
+};
+
+
+/*
+const http = require('http');
 let app = http.createServer();
 
 // Start the server on port 3000
@@ -14,5 +47,5 @@ const game = new Game(prototypeQuestions);
 //const newRound = 
 // Start game
 game.start();
-
+*/
 
